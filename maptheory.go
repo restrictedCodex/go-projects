6e197dfb5c6189b70af8d7bd7aa046d0f2b5e41d
Charudutta <restrictedcharudutta@gmail.com>
2022-09-28 03:08:08 +0530

    map and structs

---
+++ maptheory.go
@@ -0,0 +1,27 @@
+package main
+
+    // import (
+    // 	"fmt"
+    // )
+
+    // func main() {
+    // 	fmt.Println("Welcome to Maps in GO")
+
+    // 	languages := make(map[string]string)
+
+    // 	languages["JS"] = "javascript"
+    // 	languages["GO"] = "golang"
+    // 	languages["CP"] = "cplusplus"
+
+    // 	fmt.Println(languages)
+    // 	fmt.Println("GO stands for: ", languages["GO"])
+
+    // 	delete(languages, "JS")
+    // 	fmt.Println(languages)
+
+    // 	//loops in map
+
+    // 	for key, value := range languages {
+    // 		fmt.Printf("For a key %v, value is %v\n", key, value)
+    // 	}
+    // }
---
+++ structs.go
@@ -0,0 +1,25 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// type User struct {
+// 	Name   string
+// 	Email  string
+// 	Status bool
+// 	Age    int
+// }
+
+// func main() {
+// 	fmt.Println("Structs In golang")
+
+// 	hitesh := User{"Charu", "charu.dev", false, 21}
+// 	fmt.Println(hitesh)
+// 	fmt.Printf("Charu Details are %+v\n", hitesh)
+
+// 	if hitesh.Status {
+// 		fmt.Println("User verified !!")
+// 	}
+
+// }
